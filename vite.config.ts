409e758f70ef61f4cad8a081427fc90f4056cdfa
Valeriy Komlev <komlev88@gmail.com>
2025-09-05 09:56:22 +0200

    add analyzer, add robots

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { analyzer } from "vite-bundle-analyzer";
 
+// @ts-expect-error This is fine
+const hasAnalyzer = process.env.ANALYZE;
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), analyzer()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    hasAnalyzer
+      ? analyzer({
+          analyzerMode: "static",
+          openAnalyzer: true,
+        })
+      : undefined,
+  ],
 });
